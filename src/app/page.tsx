2138945f3cc9d95ebe6b6d52cde01b078747f797
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-05-30 21:35:52 -0300

    Finished main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,78 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <main className="home dark">
+      <span>My annoyingly colorful portfolio</span>
+      <section>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1>
+            Leonardo
+            <br />
+            Gatti
+          </h1>
+          <h2>
+            Web
+            <span> Developer</span>
+          </h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div>
+          <ul>
+            <li>
+              <a href="https://github.com/Leonss23" target="_blank">
+                <p>GitHub</p>
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>GitHub</title>
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/leonss23/" target="_blank">
+                <p>LinkedIn</p>
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>LinkedIn</title>
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href=""></a>
+              <p>CV</p>
+              <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M91.1142 11.6747L109.595 35.0289L93.6127 35.0301C92.2319 35.0302 91.1126 33.9109 91.1127 32.5301L91.1142 11.6747ZM88.1144 8.85577L88.1127 32.5301C88.1125 35.5678 90.575 38.0303 93.6127 38.0301L111.201 38.0288L111.559 115.973C111.565 117.352 110.454 118.476 109.075 118.485L19.0596 119.052C17.6732 119.061 16.5446 117.94 16.544 116.553L16.5016 11.0645C16.5011 9.67923 17.6271 8.55752 19.0124 8.56338L88.1144 8.85577Z"
+                  strokeWidth="3"
+                />
+                <circle cx="39.5776" cy="35.0774" r="3.75" strokeWidth="7.5" />
+                <mask id="path-3-inside-1_32_11" fill="white">
+                  <rect x="32.0776" y="41.5774" width="15" height="23" rx="5" />
+                </mask>
+                <rect
+                  x="32.0776"
+                  y="41.5774"
+                  width="15"
+                  height="23"
+                  rx="5"
+                  strokeWidth="15"
+                  mask="url(#path-3-inside-1_32_11)"
+                />
+                <path
+                  d="M54.7026 57.5774H99.0776M28.0776 77.7142H99.0776M28.0776 97.5774H99.0776"
+                  strokeWidth="5"
+                />
+                <path d="M55.0776 36.5774H84.0776" strokeWidth="5" />
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
